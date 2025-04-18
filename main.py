c278c3b17955959e69813eb7125f5f1be2ba6f64
Aakash Solanki <tech2aks@gmail.com>
2025-04-19 02:29:55 +0530

    add origins

--- main.py
+++ main.py
@@ -17,9 +17,15 @@ def get_app() -> FastAPI:
         lifespan=lifespan,
     )
 
+    origins = [
+        "*",
+        "http://localhost",
+        "http://localhost:3000",
+    ]
+
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],
+        allow_origins=origins,
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
